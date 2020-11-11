c270559016007db064faece74aa5ddec56cc31af
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2020-11-11 18:56:35 +0530

    del api

--- app/controllers/tutorialController.js
+++ app/controllers/tutorialController.js
@@ -54,4 +54,18 @@ router.put("/:id", (req, res) => {
     }
   );
 });
+
+//*DELETE api
+router.delete("/:id", (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("No record with given id : " + req.params.id);
+
+  Tutorial.findByIdAndRemove(req.params.id, (err, docs) => {
+    if (!err) res.send(docs);
+    else
+      console.log(
+        "Error while deleting a record : " + JSON.stringify(err, undefined, 2)
+      );
+  });
+});
 module.exports = router;
