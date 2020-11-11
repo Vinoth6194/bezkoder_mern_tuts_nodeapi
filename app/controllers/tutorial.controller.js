8cc60cbbf0166ef01f2800539ae0b9e10617f078
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2020-11-11 15:32:20 +0530

    get api

--- app/controllers/tutorial.controller.js
+++ app/controllers/tutorial.controller.js
@@ -2,22 +2,21 @@ const db = require("../models");
 const Tutorial = db.tutorials;
 
 // Create and Save a new Tutorial
-exports.create = (req, res) => {};
-
 // Retrieve all Tutorials from the database.
-exports.findAll = (req, res) => {};
+exports.findAll = (req, res) => {
+  const title = req.query.title;
+  var condition = title
+    ? { title: { $regex: new RegExp(title), $options: "i" } }
+    : {};
 
-// Find a single Tutorial with an id
-exports.findOne = (req, res) => {};
-
-// Update a Tutorial by the id in the request
-exports.update = (req, res) => {};
-
-// Delete a Tutorial with the specified id in the request
-exports.delete = (req, res) => {};
-
-// Delete all Tutorials from the database.
-exports.deleteAll = (req, res) => {};
-
-// Find all published Tutorials
-exports.findAllPublished = (req, res) => {};
+  Tutorial.find(condition)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials.",
+      });
+    });
+};
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+require("./app/routes/tutorial.routes")(app);
 
 var corsOptions = {
   origin: "http://localhost:8081",
